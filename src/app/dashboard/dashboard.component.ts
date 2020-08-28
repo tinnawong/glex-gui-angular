32ac42f1530b3614ccf10ecd56db628e9fae99cc
Rayy <Bowrayy@gmail.com>
2020-08-28 17:12:15 +0700

    filter finished,begin write file text and html

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable,interval  } from 'rxjs';
 import { ServiceApiService } from '../shared/service-api.service';
 import { HttpEventType, HttpResponse, HttpClient } from '@angular/common/http';
 import { ColorPickerService, Cmyk } from 'ngx-color-picker';
+import { data } from 'jquery';
 
 @Component({
 	selector: 'app-dashboard',
@@ -16,15 +17,26 @@ export class DashboardComponent implements OnInit {
 
 	constructor(private http: HttpClient, private service: ServiceApiService) { }
 
+	datetime:any
+
 	ngOnInit(): void {
+	//show data time
+	interval(1000).subscribe(x => {
+		this.datetime = Date.now()
+	});
 
 	}
+
+
 	selectedFiles: FileList = null;
 	progressInfos = [];
 	message = '';
 	fileInfos!: Observable<any>;
 	bufferdata !: Array<any>
 	ch = true
+
+
+
 
 	// ----------------------------start function uplaod--------------------------------------------
 	selectFiles(event): void {
@@ -61,30 +73,38 @@ export class DashboardComponent implements OnInit {
 	// ------------------------end function uplaod---------------------------------------------
 
 	// ------------------------start funtion filter-----------------------------------------------------
-	
-	searchStatus(nameType){
-		for (let item in this.service.filterWord) {
-			if (String(item["name"]) == nameType) {
-				return item['status']
-				
-			}
-		}
-	}
-	
-	
-	filter() {
-		this.service.resultAfterFilter = null
-		this.service.chooseSegment.forEach(data => {
-			let nameType = this.service.dictCode[data[1]]
-			// if(this.service.filterWord)
-			// console.log(data)
-			
-			if(this.searchStatus(nameType)){
-				console.log(nameType)
-			}
-			
 
+	searchStatus(nameType: string) {
+		let status: Boolean
+		this.service.filterWord.forEach(data => {
+			if (data.name == nameType) {
+				status = data.status
+				// console.log(status)
+			}
 		});
+		return status
+	}
+
+
+	filter() {
+		if (this.service.chooseSegment != null) {
+			this.service.resultAfterFilter = []
+			let dicData
+			this.service.chooseSegment.forEach(data => {
+				let nameType = this.service.dictCode[data[1]]
+				// console.log(data,nameType)	
+				// console.log(">>>status :",this.searchStatus(nameType))
+				// console.log(">>>",this.searchStatus(nameType))
+				if (this.searchStatus(nameType)) {
+					// console.log(">>>set color :",data, nameType)
+					dicData = { "data": data, "setColor": nameType }
+				} else {
+					// console.log(">>>don't set color :",data, nameType)
+					dicData = { "data": data, "setColor": "notShow" }
+				}
+				this.service.resultAfterFilter.push(dicData)
+			});
+		}
 	}
 
 
@@ -100,21 +120,76 @@ export class DashboardComponent implements OnInit {
 
 	// select file from wep page
 	openFileSegment(fileName) {
-		for (let data of this.service.results) {
-			if (data.fileName == fileName) {
-				this.service.chooseSegment = data.results
-				this.filter()
-				break
+		if (this.service.results != null) {
+			for (let data of this.service.results) {
+				if (data.fileName == fileName) {
+					this.service.chooseSegment = data.results
+					this.filter()
+					break
+				}
 			}
 		}
+
 	}
 
 	show() {
 		// console.log(this.service.results)
-		console.log(this.ch)
+		console.log(this.service.filterWord)
+	}
+
+	resetFilterWord() {
+		this.service.filterWord = [
+			{ name: "UNKNOWN", status: true, text: "คำที่ไม่รู้จัก" },
+			{ name: "KNOWN", status: true, text: "คำที่รู้จัก" },
+			{ name: "AMBIGUOUS", status: true, text: "คำกำกวม" },
+			{ name: "ENGLISH", status: true, text: "ภาษาอังกฤษ" },
+			{ name: "DIGIT", status: true, text: "ตัวเลข" },
+			{ name: "SPECIAL", status: true, text: "อักขระพิเศษ" },
+			{ name: "GROUP", status: true, text: "เครื่องหมาย" },
+		]
+		this.service.colorDict = {
+			UNKNOWN: "#FF1100",
+			KNOWN: "#00AB36",
+			AMBIGUOUS: "#0800FF",
+			ENGLISH: " #E7B635",
+			DIGIT: " #7741AF",
+			SPECIAL: "#FF7614",
+			GROUP: "#FF14F3",
+			notShow: "#B7B7B7"
+		}
+	}
+
+	writeFileText() {
+		// example = [ { "data": [ "ทดสอบ", 2 ], "setColor": "AMBIGUOUS" }, { "data": [ "การใช้งาน", 2 ], "setColor": "notShow" },]
+		let text = '';
+		this.service.resultAfterFilter.forEach(element => {
+			if ((element["setColor"] != "notShow") && (element.data[0] != " ")) {
+				text += element.data[0] + "\n";
+			}
+
+		});
+		this.downloadContent("text.txt", text)
+	}
+
+	writeFileHtml() {
+		// example = [ { "data": [ "ทดสอบ", 2 ], "setColor": "AMBIGUOUS" }, { "data": [ "การใช้งาน", 2 ], "setColor": "notShow" },]
+		// let text = '';
+		// this.service.resultAfterFilter.forEach(element => {
+		// 	if ((element["setColor"] != "notShow") && (element.data[0] != " ")) {
+		// 		text += element.data[0] + "\n";
+		// 	}
+
+		// });
+		// this.downloadContent("text.txt", text)
 	}
 
 
-
+	downloadContent(name, content) {
+		const atag = document.createElement('a');
+		const file = new Blob([content], { type: 'text/plain' });
+		atag.href = URL.createObjectURL(file);
+		atag.download = name;
+		atag.click();
+	}
 
 }
--- src/app/shared/service-api.service.ts
+++ src/app/shared/service-api.service.ts
@@ -30,44 +30,37 @@ export class ServiceApiService implements OnInit {
     { name: "GROUP", status: true, text: "เครื่องหมาย" },
   ]
 
+  resetFilterWord = [
+    { name: "UNKNOWN", status: true, text: "คำที่ไม่รู้จัก" },
+    { name: "KNOWN", status: true, text: "คำที่รู้จัก" },
+    { name: "AMBIGUOUS", status: true, text: "คำกำกวม" },
+    { name: "ENGLISH", status: true, text: "ภาษาอังกฤษ" },
+    { name: "DIGIT", status: true, text: "ตัวเลข" },
+    { name: "SPECIAL", status: true, text: "อักขระพิเศษ" },
+    { name: "GROUP", status: true, text: "เครื่องหมาย" },
+  ]
 
   dictCode = {
-    1: "UNKNOWN",
-    2: "KNOWN",
-    3: "AMBIGUOUS",
-    4: "ENGLISH",
-    5: "DIGIT",
-    6: "SPECIAL",
-    7: "GROUP"
+    0: "UNKNOWN",
+    1: "KNOWN",
+    2: "AMBIGUOUS",
+    3: "ENGLISH",
+    4: "DIGIT",
+    5: "SPECIAL",
+    6: "GROUP"
   }
 
   colorDict = {
-    UNKNOWN :"#FF1100",
-    KNOWN:"#00AB36",
-    AMBIGUOUS:" #0800FF",
-    ENGLISH:" #E7B635",
-    DIGIT:" #7741AF",
-    SPECIAL:"#FF7614",
-    GROUP:"#FF14F3",
-    notShow:"#B7B7B7"
+    UNKNOWN: "#FF1100",
+    KNOWN: "#00AB36",
+    AMBIGUOUS: "#0800FF",
+    ENGLISH: " #E7B635",
+    DIGIT: " #7741AF",
+    SPECIAL: "#FF7614",
+    GROUP: "#FF14F3",
+    notShow: "#B7B7B7"
   }
 
-
-//   public colorList = [
-//     { key: "flame", value: "#e45a33", friendlyName: "Flame" },
-//     { key: "orange", value: "#fa761e", friendlyName: "Orange" },
-//     { key: "infrared", value: "#ef486e", friendlyName: "Infrared" },
-//     { key: "male", value: "#4488ff", friendlyName: "Male Color" },
-//     { key: "female", value: "#ff44aa", friendlyName: "Female Color" },
-//     { key: "paleyellow", value: "#ffd165", friendlyName: "Pale Yellow" },
-//     { key: "gargoylegas", value: "#fde84e", friendlyName: "Gargoyle Gas" },
-//     { key: "androidgreen", value: "#9ac53e", friendlyName: "Android Green" },
-//     { key: "carribeangreen", value: "#05d59e", friendlyName: "Carribean Green" },
-//     { key: "bluejeans", value: "#5bbfea", friendlyName: "Blue Jeans" },
-//     { key: "cyancornflower", value: "#1089b1", friendlyName: "Cyan Cornflower" },
-//     { key: "warmblack", value: "#06394a", friendlyName: "Warm Black" },
-//   ];
-// checkList=[]
 
 
 
